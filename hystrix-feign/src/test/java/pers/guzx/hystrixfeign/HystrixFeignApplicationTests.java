22ae68d76c6efaaa895b3180dd921b2d1f665b51
guzx <hnguzx@qq.com>
2021-04-09 18:57:40 +0800

    使用zuul

---
+++ hystrix-feign/src/test/java/pers/guzx/hystrixfeign/HystrixFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.guzx.hystrixfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixFeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
