7be9b39f078d512bc5a876ec0ff20267a458c003
guzx <hnguzx@qq.com>
2021-04-09 18:56:23 +0800

    使用zuul

--- zuul-demo/src/main/java/pers/guzx/zuuldemo/ZuulDemoApplication.java
+++ zuul-demo/src/main/java/pers/guzx/zuuldemo/ZuulDemoApplication.java
@@ -2,8 +2,10 @@ package pers.guzx.zuuldemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulDemoApplication {
 
     public static void main(String[] args) {
